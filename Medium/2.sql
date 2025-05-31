c4ca48cef455715c1a54775a2f1b35899f98895e
Deeksha Hareesha Kulal <146106244+deeksha2107@users.noreply.github.com>
2025-05-30 22:58:30 -0500

    Create 2.sql

---
+++ Medium/2.sql
@@ -0,0 +1,20 @@
+--Show unique first names from the patients table which only occurs once in the list.
+--For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+
+SELECT first_name
+FROM patients
+group by first_name
+having COUNT(first_name)=1
+
+
+--or
+
+SELECT first_name
+FROM (
+    SELECT
+      first_name,
+      count(first_name) AS occurrencies
+    FROM patients
+    GROUP BY first_name
+  )
+WHERE occurrencies = 1
