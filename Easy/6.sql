ac8c7536524920dd601f7c1d4b6daabea9b9b5f5
Deeksha Hareesha Kulal <146106244+deeksha2107@users.noreply.github.com>
2025-05-30 20:12:28 -0500

    Update 6.sql

--- Easy/6.sql
+++ Easy/6.sql
@@ -1,3 +1,6 @@
 -- Show first name and last name concatinated into one column to show their full name.
 SELECT CONCAT(first_name,' ',last_name)  AS full_name
 FROM patients
+--or
+SELECT first_name || ' ' || last_name
+FROM patients;
